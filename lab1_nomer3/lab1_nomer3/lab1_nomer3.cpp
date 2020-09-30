fc95ddc2509642a7cd058f691002658863d77cfa
Ivan Kashcheev <basiliris69@gmail.com>
2020-09-30 21:45:27 +0300

    12

---
+++ lab1_nomer3/lab1_nomer3/lab1_nomer3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+
+	setlocale(LC_ALL, "Rus");
+	system("chcp 1251");
+	float x, y, s, p;
+	printf("введите стороны прямоугольника:");
+	scanf_s("%f", &x);
+	scanf_s("%f", &y);
+	s = x * y; //площадь
+	p = 2 * (x + y); //периметр
+	printf("площадь %f\n", s);
+	printf("периметр %f", p);
+	return 0;
+}
