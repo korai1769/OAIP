94a7305361a9c337ff8ce75db170449a5373cfca
Ivan Kashcheev <basiliris69@gmail.com>
2020-09-30 21:32:35 +0300

    123

--- lab1_nomer1/lab1_nomer1/lab1_nomer1.cpp
+++ lab1_nomer1/lab1_nomer1/lab1_nomer1.cpp
@@ -2,17 +2,16 @@
 #include <locale.h>
 #include <math.h>
 
-int main() {
+int main(){
 
-	setlocale(LC_ALL, "Russian");
-	system("chcp 1251");
-	float x, y, s, p;
-	printf("введите стороны прямоугольника:");
-	scanf_s("%f", &x);
-	scanf_s("%f", &y);
-	s = x * y; //площадь
-	p = 2 * (x + y); //периметр
-	printf("площадь %f\n", s);
-	printf("периметр %f", p);
-	return 0;
+setlocale(LC_ALL, "Russian");
+float x, y, s, p;
+printf("введите стороны прямоугольника:");
+scanf_s("%f", &x );
+scanf_s("%f", &y);
+s=x*y; //площадь
+p=2*(x+y); //периметр
+printf("площадь %f\n",s);
+printf("периметр %f",p);
+return 0;
 }
